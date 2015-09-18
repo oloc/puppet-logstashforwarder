0ec1ffd6a6a7632af78e9a956e9a8c2a24eb1904
oloc <oloc@aurore>
2015-09-18 09:27:39 +0200

    Apply workaround rodjek/rspec-puppet#175

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,10 @@ end
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + '/../'))
 
 RSpec.configure do |c|
+  c.before do
+    # avoid "Only root can execute commands as other users"
+    Puppet.features.stubs(:root? => true)
+  end
   c.add_setting :fixture_path, :default => fixture_path
   c.mock_with(:rspec)
 #  c.hiera_config = File.join(fixture_path, '/hiera/hiera.yaml')
